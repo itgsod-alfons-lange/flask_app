aa3a096f3d59da61fa327e6a3fa91a3749b57579
alfons.lange <alfons.lange@elev.it-gymnasiet.se>
2014-11-06 16:25:39 +0100

    First commit, sl API added

--- flask_app.py
+++ flask_app.py
@@ -13,6 +13,17 @@ def welcome():
 def hello_user(username):
     return render_template('user.html', username=username, title="hackerspace")
 
+@app.route('/getdata/<place>')
+def get_data(place):
+    import json, urllib
+
+    json_place = urllib.urlopen("http://alfonslange.se/sl/getPlaces.php?searchstring=%s" % place)
+
+    response = json.loads(json_place.read())['ResponseData']
+
+    return render_template('getdata.html', data=response, title="JSON-test")
+
+
 @app.route('/fakultet/<tal>')
 def count_fakultet(tal):
 
